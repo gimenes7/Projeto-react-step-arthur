5bb90a15b1abdaa2cf7ccdf7be97535bac047e80
gimenes7 <arthurgimenescosta222@gmail.com>
2026-08-08 09:50:16 -0300

    Implementa PlacarDoJogo

--- meu-react/src/App.jsx
+++ meu-react/src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Welcome from "./components/aula1/welcome";
+import PlacarDoJogo from "./components/aula2/PlacarDoJogo";
 
 function App() {
   return (
     <div>
       <Welcome name="Arthur" />
       <Contador />
+      <PlacarDoJogo />
     </div>
   );
 }
