5fb5aecb61c19a95797cfa18e3375f3fd6d7ed5d
gimenes7 <arthurgimenescosta222@gmail.com>
2026-08-15 09:23:14 -0300

    aula 5 renderizacao condicional

--- meu-react/src/components/LoginStatus.jsx
+++ meu-react/src/components/LoginStatus.jsx
@@ -1,20 +1,14 @@
 import { useState } from "react";
 import LoginStatus from "./LoginStatus";
-import Notification from "./Notification";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [showNotif, setShowNotif] = useState(false);
 
   return (
     <div>
       <LoginStatus isLoggedIn={isLoggedIn} />
-      <Notification showNotification={showNotif} />
       <button onClick={() => setIsLoggedIn(!isLoggedIn)}>
         {isLoggedIn ? "Logout" : "Login"}
-      </button>
-      <button onClick={() => setShowNotif(!showNotif)}>
-        {showNotif ? "Ocultar" : "Mostrar"} Notificação
       </button>
     </div>
   );
