cc68a12bd21c0bd5a8673cff8a6908841a68706d
gimenes7 <arthurgimenescosta222@gmail.com>
2026-08-22 10:25:40 -0300

    Fazendo um novo exercicio

--- meu-react/src/App.jsx
+++ meu-react/src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import ListaDeProdutos from "./components/aula4/ListaDeProdutos";
+import Exemplo from "./components/aula4/Exemplo";
 
 function App() {
   return (
     <div>
-      <ListaDeProdutos />
+      <Exemplo />
     </div>
   );
 }
