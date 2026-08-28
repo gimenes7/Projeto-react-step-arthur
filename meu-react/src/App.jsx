19e04e163ddca7f3d916d7452c2e830c85e1facf
gimenes7 <arthurgimenescosta222@gmail.com>
2026-08-28 18:26:24 -0300

    dever de casa

--- meu-react/src/App.jsx
+++ meu-react/src/App.jsx
@@ -1,21 +1,9 @@
-import { useState } from "react";
-import LoginStatus from "./components/LoginStatus";
-import Notification from "./components/Notification";
+import ListaFiltrada from "./components/aula4/ListaFiltrada";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [showNotif, setShowNotif] = useState(false);
-
   return (
     <div>
-      <LoginStatus isLoggedIn={isLoggedIn} />
-      <Notification showNotification={showNotif} />
-      <button onClick={() => setIsLoggedIn(!isLoggedIn)}>
-        {isLoggedIn ? "Logout" : "Login"}
-      </button>
-      <button onClick={() => setShowNotif(!showNotif)}>
-        {showNotif ? "Ocultar" : "Mostrar"} Notificação
-      </button>
+      <ListaFiltrada />
     </div>
   );
 }
