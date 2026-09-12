09c5262e630f50a1e0b5d351f780097d41672f5e
Arthur Gimenes <arthurgimenescosta222@gmail.com>
2026-09-12 10:05:27 -0300

    .

--- meu-react/src/App.jsx
+++ meu-react/src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import PlacarDoJogo from "./components/PlacarDoJogo";
-import ExemploPlacarJogo from "./components/exemploPlacar";
-import Relogio from "./components/dever de casa/ex1";
+import PlacarDoJogo from "./components/aula4PlacarDoJogo";
+import ExemploPlacarJogo from "./components/aula4/exemploPlacar";
+import Relogio from "./components/aula4/dever de casa/ex1";
 
 function App() {
   return (
