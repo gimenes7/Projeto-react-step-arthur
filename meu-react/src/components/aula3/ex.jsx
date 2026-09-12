b7ee103b0160a414018444a34fc8a33cc0050cf4
Arthur Gimenes <arthurgimenescosta222@gmail.com>
2026-09-12 09:35:22 -0300

    adicionando as outras aulas

---
+++ meu-react/src/components/aula3/ex.jsx
@@ -0,0 +1,12 @@
+const Filho = ({ message, sendDataToParent }) => {
+  return (
+    <div>
+      <p>Mensagem do pai: {message}</p>
+      <button onClick={() => sendDataToParent("Dados do Filho")}>
+        Enviar dados para o pai
+      </button>
+    </div>
+  );
+};
+
+export default Filho;
