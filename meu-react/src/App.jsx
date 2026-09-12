ce0002f875336833935cf9450c8e0e6d5020a7c6
Arthur Gimenes <arthurgimenescosta222@gmail.com>
2026-09-12 09:58:26 -0300

    adicionando as outras aulas

--- meu-react/src/App.jsx
+++ meu-react/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
-import LoginStatus from "./components/LoginStatus";
-import Notification from "./components/Notification";
+import LoginStatus from "./components/aula5/LoginStatus";
+import Notification from "./components/aula5/Notification";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
---
+++ meu-react/src/components/aula4/ex02.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import Filho from "./ex";
+
+const Pai = () => {
+  const [dadosDoFilho, setDadosDoFilho] = useState("");
+
+  const receberDados = (dados) => {
+    setDadosDoFilho(dados);
+  };
+
+  return (
+    <div>
+      <h2>Componente Pai</h2>
+      <p>Dados recebidos do filho: {dadosDoFilho}</p>
+      <Filho message="Olá do Pai!" sendDataToParent={receberDados} />
+    </div>
+  );
+};
+
+export default Pai;
